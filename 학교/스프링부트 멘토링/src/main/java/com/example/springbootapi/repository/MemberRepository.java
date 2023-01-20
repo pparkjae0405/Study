2ce15b7c3944116645efb69ea1f21a93000fd234
Jaemin Park <pparkjae0405@naver.com>
2023-01-20 12:54:08 +0900

    폴더 정리

--- 학교/스프링부트 멘토링/src/main/java/com/example/springbootapi/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootapi.repository;
-
-import com.example.springbootapi.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    
-}
